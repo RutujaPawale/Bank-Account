566442cdd49085815495d0a03aa7f5c1ac16deac
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-04 16:38:15 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,4 +3,10 @@ def balanceInquiry():
     print("Your current balance is:",balance,"\n \n")
     repeat()
 
-
+def cashDeposit():
+    global balance
+    dep_amt=int(input("Enter the amount you want to deposit:"))
+    balance=balance+dep_amt
+    print("Cash deposited successfully!! \n")
+    balanceInquiry()
+    repeat()
